c47b35d68437fe54f54e04ec871e94c52f6ded7d
Victor Passapera <vpassapera@gmail.com>
2017-04-20 17:00:32 -0700

    Changed git character.

--- candy-alt.theme.bash
+++ candy-alt.theme.bash
@@ -7,7 +7,9 @@ SCM_THEME_PROMPT_SUFFIX=""
 
 SCM_THEME_PROMPT_DIRTY=" ${bold_red}✗${normal}"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓${normal}"
-SCM_GIT_CHAR="${bold_green}⑆${normal}"
+SCM_GIT_CHAR="${bold_green}㆟${normal}"
+SCM_SVN_CHAR="${bold_cyan}⑆${normal}"
+SCM_HG_CHAR="${bold_red}☿${normal}"
 
 modern_scm_prompt() {
         CHAR=$(scm_char)
